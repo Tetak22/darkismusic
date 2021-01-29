88c2ce7fcb971c4c40206fc96112cc9c3de2e4fc
Tetak22 <76856548+Tetak22@users.noreply.github.com>
2021-01-29 11:58:45 +0100

    music

---
+++ main.js
@@ -0,0 +1,109 @@
+const { executionAsyncResource } = require('async_hooks');
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+const { YTSearcher } = require('ytsearcher');
+
+const searcher = new YTSearcher({
+    key: process.env.youtube_api,
+    revealed: true
+});
+
+const client = new Discord.Client();
+
+const queue = new Map();
+
+client.on("ready", () => {
+    console.log("Darkis music bot je online!")
+})
+
+client.on("message", async(message) => {
+    const prefix =a
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase();
+
+    switch(command){
+        case 'p':
+            execute(message, serverQueue);
+            break;
+        case 'stop':
+            stop(message, serverQueue);
+            break;
+        case 's':
+            skip(message, serverQueue);
+            break;
+    }
+
+    async function execute(message, serverQueue){
+        let vc = message.member.voice.channel;
+        if(!vc){
+            return message.channel.send("Molim te prvo udi u call!");
+        }else{
+            let result = await searcher.search(args.join(" "), { type: "video" })
+            const songInfo = await ytdl.getInfo(result.first.url)
+
+            let song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url
+            };
+
+            if(!serverQueue){
+                const queueConstructor = {
+                    txtChannel: message.channel,
+                    vChannel: vc,
+                    connection: null,
+                    songs: [],
+                    volume: 10,
+                    playing: true
+                };
+                queue.set(message.guild.id, queueConstructor);
+
+                queueConstructor.songs.push(song);
+
+                try{
+                    let connection = await vc.join();
+                    queueConstructor.connection = connection;
+                    play(message.guild, queueConstructor.songs[0]);
+                }catch (err){
+                    console.error(err);
+                    queue.delete(message.guild.id);
+                    return message.channel.send(`Nemogu uci u call ${err}`)
+                }
+            }else{
+                serverQueue.songs.push(song);
+                return message.channel.send(`Pijesma je dodana u que ${song.url}`);
+            }
+        }
+    }
+    function play(guild, song){
+        const serverQueue = queue.get(guild.id);
+        if(!song){
+            serverQueue.vChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url))
+            .on('finish', () =>{
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            serverQueue.txtChannel.send(`:thumbsup:Sada slusamo ${serverQueue.songs[0].url}`)
+    }
+    function stop (message, serverQueue){
+        if(!message.member.voice.channel)
+            return message.channel.send("Moras uci u call prvo!")
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    }
+    function skip (message, serverQueue){
+        if(!message.member.voice.channel)
+            return message.channel.send("Moras biti u callu da bi skipo");
+        if(!serverQueue)
+            return message.channel.send("Nema nista za skipati!");
+        serverQueue.connection.dispatcher.end();
+    }
+})
+
+client.login(process.env.token)
